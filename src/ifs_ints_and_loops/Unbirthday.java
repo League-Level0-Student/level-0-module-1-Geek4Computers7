0e610bb32d86efee49062d69a29f725a85c4721e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-12 17:00:02 -0700

    day2

--- src/ifs_ints_and_loops/Unbirthday.java
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -1,5 +1,11 @@
 package ifs_ints_and_loops;
 
+import javax.swing.JOptionPane;
+
 public class Unbirthday {
+	public static void main(String[] args) {
+String birth = 	JOptionPane.showInputDialog("what is your birth month and day");
+		
+	}
 
 }
